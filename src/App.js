9bb0fb0cc577f2b5319b57b8b446b2f5c30f2a2c
aguescribano87 <aguescribano87@gmail.com>
2021-07-27 12:04:25 -0300

    calc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [result, setResult] = useState("")
+  const [operadorA, setOperadorA] = useState("")
+  const [operadorB, setOperadorB] = useState("")
+  const [operacion, setOperacion] = useState("")
+  
+  function reset(){
+    setResult("")
+  }
+
+  function calculo(operador){
+    const A = result
+    setOperadorA(A)
+    switch (operador) {
+      case "+":
+        setOperacion(operador)
+        break;
+      case "-":
+        setOperacion(operador)
+        break;
+      case "*":
+        setOperacion(operador)
+        break;
+      case "/":
+        setOperacion(operador)
+        break;
+    
+      default:
+        break;
+    }
+
+    reset()
+  }
+
+  function igual(){
+    let res
+    const B = result
+    operadorB(B)
+    setOperadorB(B)
+    switch (operacion) {
+      case "+":
+        res = parseInt(operadorA) + parseInt(B)
+        break;
+      case "-":
+        res = parseInt(operadorA) - parseInt(B)
+        break;
+      case "*":
+        res = parseInt(operadorA) * parseInt(B)
+        break;
+      case "/":
+        res = parseInt(operadorA) / parseInt(B)
+        break;
+    
+      default:
+        break;
+    }
+    setResult(res)
+    
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculadora">
+      <table>
+        <tr>
+          <td colSpan="4"><span><p id="resultado" type="number" >{result}</p></span></td>
+        </tr>
+        <tr>
+          <td><button onClick={()=>(setResult(result+"7"))}>7</button></td>
+          <td><button onClick={()=>(setResult(result+"8"))}>8</button></td>
+          <td><button onClick={()=>(setResult(result+"9"))}>9</button></td>
+          <td><button onClick={()=>{calculo("/")}}>/</button></td>
+        </tr>
+        <tr>
+          <td><button onClick={()=>(setResult(result+"4"))}>4</button></td>
+          <td><button onClick={()=>(setResult(result+"5"))}>5</button></td>
+          <td><button onClick={()=>(setResult(result+"6"))}>6</button></td>
+          <td><button onClick={()=>{calculo("*")}}>*</button></td>
+        </tr>
+        <tr>
+          <td><button onClick={()=>(setResult(result+"1"))}>1</button></td>
+          <td><button onClick={()=>(setResult(result+"2"))}>2</button></td>
+          <td><button onClick={()=>(setResult(result+"3"))}>3</button></td>
+          <td><button onClick={()=>{calculo("-")}}>-</button></td>
+        </tr>
+        <tr>
+          <td><button onClick={igual}>=</button></td>
+          <td><button onClick={reset}>C</button></td>
+          <td><button onClick={()=>(setResult(result+"0"))}>0</button></td>
+          <td><button onClick={()=>{calculo("+")}}>+</button></td>
+        </tr>
+      </table>
     </div>
   );
 }
